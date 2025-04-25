8368f28f75f8fb7b7ce35873c5a37028a93dadad
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2025-04-25 11:03:17 +0200

    Skip ts checking

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-function addViteIgnore() {
-  return {
-    name: "add-vite-ignore",
-    closeBundle() {
-      const filePath = path.resolve(__dirname, "dist/assets/remoteEntry.js");
-      let content = fs.readFileSync(filePath, "utf-8");
-      content = content.replace(
-        /import\(([^)]+)\)/g,
-        "import(/* @vite-ignore */ $1)"
-      );
-      fs.writeFileSync(filePath, content);
-    },
-  };
-}
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), addViteIgnore()],
+  plugins: [react()],
 })
